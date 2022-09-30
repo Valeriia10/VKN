0f55810fc5a13ecd8d76ea20e0317a9823286701
ValeriiaKravchuk0 <113578688+Valeriia10@users.noreply.github.com>
2022-09-30 12:11:38 +0300

    Add files via upload

---
+++ pr4.1.py
@@ -0,0 +1,5 @@
+import math
+x=float(input("введіть х"))
+y=math.pow(3,-x)*math.sqrt(math.fabs(3.19*x))+(math.log(math.fabs(x+1),48)/(math.fabs(x+1)+0.7))
+print(y)
+input(".")
---
+++ pr4.2.py
@@ -0,0 +1,12 @@
+import math
+def func (a,b,e,c,x):
+         X=math.sqrt(a*math.cos(b)+math.e**c)+math.log(math.pow(2,x)+4,7)
+         return(X)
+a1=float(input("введіть зміну a: "))  
+b1=float(input("введіть зміну b: "))    
+e1=float(input("введіть зміну e: "))
+c1=float(input("введіть зміну c: "))
+x1=float(input("введіть зміну x: "))
+f=func(a1,b1,e1,c1,x1)
+print(f)
+input("відповідь.")
